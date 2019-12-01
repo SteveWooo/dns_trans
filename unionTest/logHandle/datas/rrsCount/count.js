c578278ce64ffc3e523d39326e0d0c09a62683f1
stevewooo <461437874@qq.com>
2019-12-02 03:56:26 +0800

    a

--- unionTest/logHandle/datas/rrsCount/count.js
+++ unionTest/logHandle/datas/rrsCount/count.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 function handleFile(options) {
     return new Promise(resolve=>{
         var rl = readline.createInterface({
-            input : fs.createReadStream(options.filename)
+            input : fs.createReadStream(options.filepath)
         })
         var result = {
             count : 0,
@@ -12,9 +12,9 @@ function handleFile(options) {
             avgDelay : 0,
         };
         rl.on('close', function(){
-            console.log(`finished ${options.filename}`);
+            // console.log(`finished ${options.dir} ${options.filename}`);
             result.avgDelay = result.allDelay / result.count;
-            console.log(result);
+            // console.log(result);
             resolve(result);
         })
 
@@ -22,22 +22,60 @@ function handleFile(options) {
             result.count ++;
             data = data.split('\`');
             var delay = parseInt(data[0].split('=')[1]);
+            // if(delay > 40) {
+            //     return ;
+            // }
             result.allDelay += delay;
         })
     })
 }
 
-async function main(){
-    var dirs = fs.readdirSync(`${__dirname}/sourceLogs`);
+async function main(options){
+    var result = {};
+    var dirs = fs.readdirSync(`${__dirname}/${options.type}Source`);
     for(var i=0;i<dirs.length;i++){
         var dirname = dirs[i];
-        var subDir = fs.readdirSync(`${__dirname}/sourceLogs/${dirname}`);
+        result[dirname] = {
+            delays : [],
+            avgDelay : 0,
+        };
+        var subDir = fs.readdirSync(`${__dirname}/${options.type}Source/${dirname}`);
         for(var k=0;k<subDir.length;k++){
             var filename = subDir[k];
-            var result = await handleFile({
-                filename : `${__dirname}/sourceLogs/${dirname}/${filename}`
+            var temp = await handleFile({
+                filepath : `${__dirname}/${options.type}Source/${dirname}/${filename}`,
+                filename : filename,
+                dir : dirname
             })
+            result[dirname].delays.push(temp.avgDelay);
         }
+
+        var tempAllDelay = 0;
+        for(var d=0;d<result[dirname].delays.length;d++){
+            tempAllDelay += result[dirname].delays[d];
+        }
+        result[dirname].avgDelay = tempAllDelay / result[dirname].delays.length;
+
     }
+
+    var file = [];
+    var fileObjects = [];
+    for(var i in result) {
+        var x = parseInt(i.replace('k', ''));
+        fileObjects.push({
+            x : x,
+            avgDelay: result[i].avgDelay
+        })
+    }
+    fileObjects = fileObjects.sort(function(a, b){
+        return a.x > b.x
+    })
+    for(var i=0;i<fileObjects.length;i++){
+        file.push(`${fileObjects[i].x} ${fileObjects[i].avgDelay}`);
+    }
+
+    fs.writeFileSync(`${__dirname}/resultForGlunt/${options.type}`, file.join('\n'));
 }
-main();
+main({
+    type : 'recursive'
+});
---
+++ unionTest/logHandle/datas/rrsCount/resultForGlunt/buildImage.sh
@@ -0,0 +1,14 @@
+gnuplot -persist <<-EOFMarker
+	set yrange [0:8]
+	set xlabel "Miss Rate" font 'times.ttf,18'
+	set ylabel "Delay" font 'times.ttf,18'
+	set grid
+	set xtics font 'times.ttf,10'
+	set ytics font 'times.ttf,10'
+	set key bottom at 90, 7 font 'times.ttf,18'
+	plot"recursive" using 1:2 w lp pt 7 title "normal"
+	set terminal postscript eps color
+	#set output "./output rate.eps"
+	replot
+	set output
+EOFMarker
